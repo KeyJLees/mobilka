b5e268f97eacb70141520cfefd5142434e47b10d
KeyJLees <64843905+KeyJLees@users.noreply.github.com>
2020-05-08 14:25:07 +0700

    добавил запрос разрешения

--- MyApplication2/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication2/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,25 +1,27 @@
 package com.blogspot.atifsoftwares.imagepick_kotlin
 
-import android.Manifest
 import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.os.Build
-import android.os.Build.*
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.jar.Manifest
+
 
 class MainActivity : AppCompatActivity() {
 
+    private val CAMERA_PERMISSION_CODE = 100;
+    private val STORAGE_PERMISSION_CODE = 101;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         //BUTTON CLICK
         img_pick_btn.setOnClickListener {
-
+            checkPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE, STORAGE_PERMISSION_CODE)
                     //permission already granted
                     pickImageFromGallery();
                 }
@@ -35,6 +37,84 @@ class MainActivity : AppCompatActivity() {
             rotateImageLeft();
         }
 
+    }
+
+    private fun checkPermission(permission: String, requestCode: Int) {
+
+        // Проверка, если разрешение не предоставлено
+        if (ContextCompat.checkSelfPermission(
+                this@MainActivity,
+                permission
+            )
+            == PackageManager.PERMISSION_DENIED
+        ) {
+            ActivityCompat
+                .requestPermissions(
+                    this@MainActivity, arrayOf(permission),
+                    requestCode
+                )
+        } else {
+            Toast
+                .makeText(
+                    this@MainActivity,
+                    "Permission already granted",
+                    Toast.LENGTH_SHORT
+                )
+                .show()
+        }
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String?>,
+        grantResults: IntArray
+    ) {
+        super
+            .onRequestPermissionsResult(
+                requestCode,
+                permissions,
+                grantResults
+            )
+        if (requestCode == CAMERA_PERMISSION_CODE) {
+
+            // Проверка, предоставил ли пользователь разрешение или нет.
+            if (grantResults.size > 0
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED
+            ) {
+
+                // Отображение тостового сообщения
+                Toast.makeText(
+                    this@MainActivity,
+                    "Camera Permission Granted",
+                    Toast.LENGTH_SHORT
+                )
+                    .show()
+            } else {
+                Toast.makeText(
+                    this@MainActivity,
+                    "Camera Permission Denied",
+                    Toast.LENGTH_SHORT
+                )
+                    .show()
+            }
+        } else if (requestCode == STORAGE_PERMISSION_CODE) {
+            if (grantResults.size > 0
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED
+            ) {
+                Toast.makeText(
+                    this@MainActivity,
+                    "Storage Permission Granted",
+                    Toast.LENGTH_SHORT
+                )
+                    .show()
+            } else {
+                Toast.makeText(
+                    this@MainActivity,
+                    "Storage Permission Denied",
+                    Toast.LENGTH_SHORT
+                )
+                    .show()
+            }
+        }
     }
 
     private fun pickImageFromGallery() {
