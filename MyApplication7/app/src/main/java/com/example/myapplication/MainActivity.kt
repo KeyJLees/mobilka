4ce75e20afd47a2f895d9b3d09eeb20c6f9f55d2
KeyJLees <64843905+KeyJLees@users.noreply.github.com>
2020-05-18 19:52:24 +0700

    filters

--- MyApplication7/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication7/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,12 +5,14 @@ import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.graphics.Color
 import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.DisplayMetrics
 import android.view.Menu
 import android.widget.ImageView
+import android.widget.SeekBar
 import android.widget.Toast
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
@@ -25,6 +27,9 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.graphics.blue
+import androidx.core.graphics.green
+import androidx.core.graphics.red
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.math.*
 
@@ -35,8 +40,8 @@ class MainActivity : AppCompatActivity() {
     private val CAMERA = 2;
     private val CAMERA_PERMISSION_CODE = 100;
     private val STORAGE_PERMISSION_CODE = 101;
-    private val imageview: ImageView? = null
-
+    private lateinit var gbmap: Bitmap
+    private var brightt: Double = 1.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -76,6 +81,42 @@ class MainActivity : AppCompatActivity() {
 
             rottateImage(image_view, 270);
         }
+
+        button3.setOnClickListener {
+
+            negative()
+        }
+
+        button4.setOnClickListener {
+
+            whiteblack()
+        }
+
+        button5.setOnClickListener {
+
+            gray()
+        }
+
+        button6.setOnClickListener {
+
+            sepia()
+        }
+
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+
+            override fun onProgressChanged(seekBar: SeekBar, i: Int, b: Boolean) {
+                bright(seekBar.progress / 10.0)
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+
+            }
+        })
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -194,6 +235,19 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE){
+            image_view.setImageURI(data?.data)
+            gbmap = (image_view.getDrawable() as BitmapDrawable).bitmap
+        }
+        else if (resultCode == Activity.RESULT_OK && requestCode == CAMERA){
+            val thumbnail = data!!.extras!!["data"] as Bitmap
+            image_view.setImageBitmap(thumbnail);
+            gbmap = (image_view.getDrawable() as BitmapDrawable).bitmap
+        }
+    }
+
     private fun rottateImage(view: ImageView, degrees: Int) {
 
         val bmap: Bitmap = (view.getDrawable() as BitmapDrawable).bitmap
@@ -237,17 +291,152 @@ class MainActivity : AppCompatActivity() {
                     bmp.setPixel(x, y, 0)
             }
         image_view.setImageBitmap(bmp);
+        gbmap = (image_view.getDrawable() as BitmapDrawable).bitmap
 
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE){
-            image_view.setImageURI(data?.data)
-        }
-        else if (resultCode == Activity.RESULT_OK && requestCode == CAMERA){
-            val thumbnail = data!!.extras!!["data"] as Bitmap
-            image_view.setImageBitmap(thumbnail);
-        }
+    private fun bright(brightness: Double) {
+        brightt = brightness
+        val bmap: Bitmap = gbmap
+        val aspectRatio: Float = bmap.height.toFloat() / bmap.width
+        val displayMetrics: DisplayMetrics = resources.displayMetrics
+        val mImageWidth = displayMetrics.widthPixels
+        val mImageHeight = (mImageWidth * aspectRatio).roundToInt()
+        val mBitmap = Bitmap.createScaledBitmap(bmap, mImageWidth, mImageHeight, false)
+
+        val nWidth: Int = mBitmap.width
+        val nHeight: Int = mBitmap.height
+        val bmp: Bitmap = Bitmap.createBitmap(nWidth, nHeight, mBitmap.config)
+        for (y in 0 until nHeight)
+            for (x in 0 until nWidth){
+                val r = mBitmap.getPixel(x, y).red
+                val g = mBitmap.getPixel(x, y).green
+                val b = mBitmap.getPixel(x, y).blue
+                var red: Int = (r * brightness).toInt()
+                red = min(255, max(0,red))
+                var green: Int = (g * brightness).toInt()
+                green = min(255, max(0,green))
+                var blue: Int = (b * brightness).toInt()
+                blue = min(255, max(0,blue))
+                bmp.setPixel(x,y,Color.rgb(red, green, blue))
+            }
+        image_view.setImageBitmap(bmp);
     }
+
+    private fun negative() {
+        val bmap: Bitmap = gbmap
+        val aspectRatio: Float = bmap.height.toFloat() / bmap.width
+        val displayMetrics: DisplayMetrics = resources.displayMetrics
+        val mImageWidth = displayMetrics.widthPixels
+        val mImageHeight = (mImageWidth * aspectRatio).roundToInt()
+        val mBitmap = Bitmap.createScaledBitmap(bmap, mImageWidth, mImageHeight, false)
+
+        val nWidth: Int = mBitmap.width
+        val nHeight: Int = mBitmap.height
+        val bmp: Bitmap = Bitmap.createBitmap(nWidth, nHeight, mBitmap.config)
+        for (y in 0 until nHeight)
+            for (x in 0 until nWidth) {
+                val r = mBitmap.getPixel(x, y).red
+                val g = mBitmap.getPixel(x, y).green
+                val b = mBitmap.getPixel(x, y).blue
+                bmp.setPixel(x,y,Color.rgb(255 - r, 255 - g, 255 - b))
+            }
+        image_view.setImageBitmap(bmp);
+
+    }
+
+    private fun whiteblack() {
+        val bmap: Bitmap = gbmap
+        val aspectRatio: Float = bmap.height.toFloat() / bmap.width
+        val displayMetrics: DisplayMetrics = resources.displayMetrics
+        val mImageWidth = displayMetrics.widthPixels
+        val mImageHeight = (mImageWidth * aspectRatio).roundToInt()
+        val mBitmap = Bitmap.createScaledBitmap(bmap, mImageWidth, mImageHeight, false)
+
+        val nWidth: Int = mBitmap.width
+        val nHeight: Int = mBitmap.height
+        val bmp: Bitmap = Bitmap.createBitmap(nWidth, nHeight, mBitmap.config)
+
+        val separator: Double = 255 / brightt / 2 * 3
+        for (y in 0 until nHeight)
+            for (x in 0 until nWidth) {
+                val r = mBitmap.getPixel(x, y).red
+                val g = mBitmap.getPixel(x, y).green
+                val b = mBitmap.getPixel(x, y).blue
+                var total: Double = (r + g + b).toDouble()
+                if (total > separator){
+                    bmp.setPixel(x,y,Color.rgb(255, 255, 255))
+                }
+                else {
+                    bmp.setPixel(x,y,Color.rgb(0, 0, 0))
+                }
+            }
+        image_view.setImageBitmap(bmp);
+    }
+
+    private fun gray() {
+        val bmap: Bitmap = gbmap
+        val aspectRatio: Float = bmap.height.toFloat() / bmap.width
+        val displayMetrics: DisplayMetrics = resources.displayMetrics
+        val mImageWidth = displayMetrics.widthPixels
+        val mImageHeight = (mImageWidth * aspectRatio).roundToInt()
+        val mBitmap = Bitmap.createScaledBitmap(bmap, mImageWidth, mImageHeight, false)
+
+        val nWidth: Int = mBitmap.width
+        val nHeight: Int = mBitmap.height
+        val bmp: Bitmap = Bitmap.createBitmap(nWidth, nHeight, mBitmap.config)
+
+        for (y in 0 until nHeight)
+            for (x in 0 until nWidth) {
+                val r = mBitmap.getPixel(x, y).red
+                val g = mBitmap.getPixel(x, y).green
+                val b = mBitmap.getPixel(x, y).blue
+                val grayy: Int = (r * 0.2126 + g * 0.7152 + b * 0.0722).toInt()
+                bmp.setPixel(x,y,Color.rgb(grayy, grayy, grayy))
+            }
+        image_view.setImageBitmap(bmp);
+    }
+
+    private fun sepia() {
+        val bmap: Bitmap = gbmap
+        val aspectRatio: Float = bmap.height.toFloat() / bmap.width
+        val displayMetrics: DisplayMetrics = resources.displayMetrics
+        val mImageWidth = displayMetrics.widthPixels
+        val mImageHeight = (mImageWidth * aspectRatio).roundToInt()
+        val mBitmap = Bitmap.createScaledBitmap(bmap, mImageWidth, mImageHeight, false)
+
+        val nWidth: Int = mBitmap.width
+        val nHeight: Int = mBitmap.height
+        val bmp: Bitmap = Bitmap.createBitmap(nWidth, nHeight, mBitmap.config)
+
+        for (y in 0 until nHeight)
+            for (x in 0 until nWidth) {
+                val r = mBitmap.getPixel(x, y).red
+                val g = mBitmap.getPixel(x, y).green
+                val b = mBitmap.getPixel(x, y).blue
+                val red: Int = (r * 0.393 + g * 0.769 + b * 0.189).toInt()
+                val green = (r * 0.349 + g * 0.686 + b * 0.168).toInt()
+                val blue = (r * 0.272 + g * 0.534 + b * 0.131).toInt()
+                bmp.setPixel(x,y,Color.rgb(red, green, blue))
+            }
+        image_view.setImageBitmap(bmp);
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
