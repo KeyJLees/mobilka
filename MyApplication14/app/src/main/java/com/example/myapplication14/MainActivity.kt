317c47255c30cf874c8bfaf3e9862899d6fde65d
Artyom <artyom.kalughin@mail.ru>
2020-05-22 20:01:26 +0700

    'ыы'

---
+++ MyApplication14/app/src/main/java/com/example/myapplication14/MainActivity.kt
@@ -0,0 +1,361 @@
+package com.example.myapplication14
+
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Color
+import android.graphics.drawable.BitmapDrawable
+import android.os.Bundle
+import android.provider.MediaStore
+import android.util.DisplayMetrics
+import android.view.View
+import android.widget.ImageView
+import android.widget.SeekBar
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.blue
+import androidx.core.graphics.green
+import androidx.core.graphics.red
+
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.image_view
+import kotlinx.android.synthetic.main.filters_main.*
+import kotlin.math.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    private val IMAGE_PICK_CODE = 1;
+    private val CAMERA = 2;
+    private val CAMERA_PERMISSION_CODE = 100;
+    private val STORAGE_PERMISSION_CODE = 101;
+    private lateinit var gbmap: Bitmap
+    private var brightt: Double = 1.0
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        //BUTTON CLICK
+        img_pick_btn.setOnClickListener {
+            checkPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE, STORAGE_PERMISSION_CODE)
+            //permission already granted
+            pickImageFromGallery(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+        img_pick_btn2.setOnClickListener {
+            checkPermission(Manifest.permission.CAMERA, CAMERA_PERMISSION_CODE)
+            //permission already granted
+            takePhoto(Manifest.permission.CAMERA);
+        }
+
+        rotate_img_left.setOnClickListener {
+            rottateImage(image_view,360-edit_text.text.toString().toInt());
+        }
+
+
+    }
+
+    fun nextSlide(view: View)
+    {
+        val randomIntent=Intent(this,SecondActivity::class.java)
+        startActivity(randomIntent)
+    }
+
+    private fun checkPermission(permission: String, requestCode: Int) {
+
+        // Проверка, если разрешение не предоставлено
+        if (ContextCompat.checkSelfPermission(
+                this@MainActivity,
+                permission
+            )
+            == PackageManager.PERMISSION_DENIED
+        ) {
+            ActivityCompat
+                .requestPermissions(
+                    this@MainActivity, arrayOf(permission),
+                    requestCode
+                )
+        } else {
+            Toast
+                .makeText(
+                    this@MainActivity,
+                    "Permission already granted",
+                    Toast.LENGTH_SHORT
+                )
+                .show()
+        }
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String?>,
+        grantResults: IntArray
+    ) {
+        super
+            .onRequestPermissionsResult(
+                requestCode,
+                permissions,
+                grantResults
+            )
+        if (requestCode == CAMERA_PERMISSION_CODE) {
+
+            // Проверка, предоставил ли пользователь разрешение или нет.
+            if (grantResults.size > 0
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED
+            ) {
+
+                // Отображение тостового сообщения
+                Toast.makeText(
+                    this@MainActivity,
+                    "Camera Permission Granted",
+                    Toast.LENGTH_SHORT
+                )
+                    .show()
+            } else {
+                Toast.makeText(
+                    this@MainActivity,
+                    "Camera Permission Denied",
+                    Toast.LENGTH_SHORT
+                )
+                    .show()
+            }
+        } else if (requestCode == STORAGE_PERMISSION_CODE) {
+            if (grantResults.size > 0
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED
+            ) {
+                Toast.makeText(
+                    this@MainActivity,
+                    "Storage Permission Granted",
+                    Toast.LENGTH_SHORT
+                )
+                    .show()
+            } else {
+                Toast.makeText(
+                    this@MainActivity,
+                    "Storage Permission Denied",
+                    Toast.LENGTH_SHORT
+                )
+                    .show()
+            }
+        }
+    }
+
+    private fun takePhoto(permission: String){
+        if (ContextCompat.checkSelfPermission(
+                this@MainActivity,
+                permission
+            )
+            == PackageManager.PERMISSION_GRANTED
+        ) {
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(intent, CAMERA)
+        }
+    }
+
+
+    private fun pickImageFromGallery(permission: String) {
+        if (ContextCompat.checkSelfPermission(
+                this@MainActivity,
+                permission
+            )
+            == PackageManager.PERMISSION_GRANTED
+        ) {
+            val gallaryintent = Intent(Intent.ACTION_PICK)
+            gallaryintent.type = "image/*"
+            startActivityForResult(gallaryintent, IMAGE_PICK_CODE)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE){
+            image_view.setImageURI(data?.data)
+            gbmap = (image_view.getDrawable() as BitmapDrawable).bitmap
+        }
+        else if (resultCode == Activity.RESULT_OK && requestCode == CAMERA){
+            val thumbnail = data!!.extras!!["data"] as Bitmap
+            image_view.setImageBitmap(thumbnail);
+            gbmap = (image_view.getDrawable() as BitmapDrawable).bitmap
+        }
+    }
+
+    private fun rottateImage(view: ImageView, degrees: Int) {
+
+        val bmap: Bitmap = (view.getDrawable() as BitmapDrawable).bitmap
+        val aspectRatio: Float = bmap.height.toFloat() / bmap.width
+        val displayMetrics: DisplayMetrics = resources.displayMetrics
+        val mImageWidth = displayMetrics.widthPixels
+        val mImageHeight = (mImageWidth * aspectRatio).roundToInt()
+        val mBitmap = Bitmap.createScaledBitmap(bmap, mImageWidth, mImageHeight, false)
+        val rad = (degrees * 3.1415926535f) / 180f
+        val cosf = cos(rad)
+        val sinf = sin(rad)
+
+        val nWidth: Int = mBitmap.width
+        val nHeight: Int = mBitmap.height
+
+        val x1 = (-nHeight * sinf).toInt()
+        val y1 = (nHeight * cosf).toInt()
+        val x2 = (nWidth * cosf - nHeight * sinf).toInt()
+        val y2 = (nHeight * cosf + nWidth * sinf).toInt()
+        val x3 = (nWidth * cosf).toInt()
+        val y3 = (nWidth * sinf).toInt()
+
+        val minX = min(0, min(x1, min(x2, x3)))
+        val minY = min(0, min(y1, min(y2, y3)))
+        val maxX = max(0, max(x1, max(x2, x3)))
+        val maxY = max(0, max(y1, max(y2, y3)))
+
+        val w = maxX - minX
+        val h = maxY - minY
+        val bmp: Bitmap = Bitmap.createBitmap(w, h, mBitmap.config)
+
+
+        for (y in 0 until h)
+            for (x in 0 until w) {
+
+                val sourceX = ((x + minX) * cosf + (y + minY) * sinf).toInt()
+                val sourceY = ((y + minY) * cosf - (x + minX) * sinf).toInt()
+                if (sourceX in 0 until nWidth && sourceY in 0 until nHeight)
+                    bmp.setPixel(x, y, mBitmap.getPixel(sourceX, sourceY))
+                else
+                    bmp.setPixel(x, y, 0)
+            }
+        image_view.setImageBitmap(bmp);
+        gbmap = (image_view.getDrawable() as BitmapDrawable).bitmap
+
+    }
+
+
+    private fun bright(brightness: Double) {
+        brightt = brightness
+        val bmap: Bitmap = gbmap
+        val aspectRatio: Float = bmap.height.toFloat() / bmap.width
+        val displayMetrics: DisplayMetrics = resources.displayMetrics
+        val mImageWidth = displayMetrics.widthPixels
+        val mImageHeight = (mImageWidth * aspectRatio).roundToInt()
+        val mBitmap = Bitmap.createScaledBitmap(bmap, mImageWidth, mImageHeight, false)
+
+        val nWidth: Int = mBitmap.width
+        val nHeight: Int = mBitmap.height
+        val bmp: Bitmap = Bitmap.createBitmap(nWidth, nHeight, mBitmap.config)
+        for (y in 0 until nHeight)
+            for (x in 0 until nWidth){
+                val r = mBitmap.getPixel(x, y).red
+                val g = mBitmap.getPixel(x, y).green
+                val b = mBitmap.getPixel(x, y).blue
+                var red: Int = (r * brightness).toInt()
+                red = min(255, max(0,red))
+                var green: Int = (g * brightness).toInt()
+                green = min(255, max(0,green))
+                var blue: Int = (b * brightness).toInt()
+                blue = min(255, max(0,blue))
+                bmp.setPixel(x,y, Color.rgb(red, green, blue))
+            }
+        image_view.setImageBitmap(bmp);
+    }
+
+    private fun negative() {
+        val bmap: Bitmap = gbmap
+        val aspectRatio: Float = bmap.height.toFloat() / bmap.width
+        val displayMetrics: DisplayMetrics = resources.displayMetrics
+        val mImageWidth = displayMetrics.widthPixels
+        val mImageHeight = (mImageWidth * aspectRatio).roundToInt()
+        val mBitmap = Bitmap.createScaledBitmap(bmap, mImageWidth, mImageHeight, false)
+
+        val nWidth: Int = mBitmap.width
+        val nHeight: Int = mBitmap.height
+        val bmp: Bitmap = Bitmap.createBitmap(nWidth, nHeight, mBitmap.config)
+        for (y in 0 until nHeight)
+            for (x in 0 until nWidth) {
+                val r = mBitmap.getPixel(x, y).red
+                val g = mBitmap.getPixel(x, y).green
+                val b = mBitmap.getPixel(x, y).blue
+                bmp.setPixel(x,y, Color.rgb(255 - r, 255 - g, 255 - b))
+            }
+        image_view.setImageBitmap(bmp);
+
+    }
+
+    private fun whiteblack() {
+        val bmap: Bitmap = gbmap
+        val aspectRatio: Float = bmap.height.toFloat() / bmap.width
+        val displayMetrics: DisplayMetrics = resources.displayMetrics
+        val mImageWidth = displayMetrics.widthPixels
+        val mImageHeight = (mImageWidth * aspectRatio).roundToInt()
+        val mBitmap = Bitmap.createScaledBitmap(bmap, mImageWidth, mImageHeight, false)
+
+        val nWidth: Int = mBitmap.width
+        val nHeight: Int = mBitmap.height
+        val bmp: Bitmap = Bitmap.createBitmap(nWidth, nHeight, mBitmap.config)
+
+        val separator: Double = 255 / brightt / 2 * 3
+        for (y in 0 until nHeight)
+            for (x in 0 until nWidth) {
+                val r = mBitmap.getPixel(x, y).red
+                val g = mBitmap.getPixel(x, y).green
+                val b = mBitmap.getPixel(x, y).blue
+                var total: Double = (r + g + b).toDouble()
+                if (total > separator){
+                    bmp.setPixel(x,y, Color.rgb(255, 255, 255))
+                }
+                else {
+                    bmp.setPixel(x,y, Color.rgb(0, 0, 0))
+                }
+            }
+        image_view.setImageBitmap(bmp);
+    }
+
+    private fun gray() {
+        val bmap: Bitmap = gbmap
+        val aspectRatio: Float = bmap.height.toFloat() / bmap.width
+        val displayMetrics: DisplayMetrics = resources.displayMetrics
+        val mImageWidth = displayMetrics.widthPixels
+        val mImageHeight = (mImageWidth * aspectRatio).roundToInt()
+        val mBitmap = Bitmap.createScaledBitmap(bmap, mImageWidth, mImageHeight, false)
+
+        val nWidth: Int = mBitmap.width
+        val nHeight: Int = mBitmap.height
+        val bmp: Bitmap = Bitmap.createBitmap(nWidth, nHeight, mBitmap.config)
+
+        for (y in 0 until nHeight)
+            for (x in 0 until nWidth) {
+                val r = mBitmap.getPixel(x, y).red
+                val g = mBitmap.getPixel(x, y).green
+                val b = mBitmap.getPixel(x, y).blue
+                val grayy: Int = (r * 0.2126 + g * 0.7152 + b * 0.0722).toInt()
+                bmp.setPixel(x,y, Color.rgb(grayy, grayy, grayy))
+            }
+        image_view.setImageBitmap(bmp);
+    }
+
+    private fun sepia() {
+        val bmap: Bitmap = gbmap
+        val aspectRatio: Float = bmap.height.toFloat() / bmap.width
+        val displayMetrics: DisplayMetrics = resources.displayMetrics
+        val mImageWidth = displayMetrics.widthPixels
+        val mImageHeight = (mImageWidth * aspectRatio).roundToInt()
+        val mBitmap = Bitmap.createScaledBitmap(bmap, mImageWidth, mImageHeight, false)
+
+        val nWidth: Int = mBitmap.width
+        val nHeight: Int = mBitmap.height
+        val bmp: Bitmap = Bitmap.createBitmap(nWidth, nHeight, mBitmap.config)
+
+        for (y in 0 until nHeight)
+            for (x in 0 until nWidth) {
+                val r = mBitmap.getPixel(x, y).red
+                val g = mBitmap.getPixel(x, y).green
+                val b = mBitmap.getPixel(x, y).blue
+                val red: Int = (r * 0.393 + g * 0.769 + b * 0.189).toInt()
+                val green = (r * 0.349 + g * 0.686 + b * 0.168).toInt()
+                val blue = (r * 0.272 + g * 0.534 + b * 0.131).toInt()
+                bmp.setPixel(x,y, Color.rgb(red, green, blue))
+            }
+        image_view.setImageBitmap(bmp);
+    }
+}
---
+++ MyApplication14/app/src/main/java/com/example/myapplication14/SecondActivity.kt
@@ -0,0 +1,30 @@
+package com.example.myapplication14
+
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Color
+import android.os.Bundle
+import android.util.DisplayMetrics
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.blue
+import androidx.core.graphics.green
+import androidx.core.graphics.red
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.filters_main.*
+import kotlin.math.max
+import kotlin.math.min
+import kotlin.math.roundToInt
+
+class SecondActivity : AppCompatActivity() {
+    
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.filters_main)
+    }
+
+
+
+
+}
