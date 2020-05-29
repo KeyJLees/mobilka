31636d762483e9c53a525ed3aca5c7400901e45f
Artyom <artyom.kalughin@mail.ru>
2020-05-30 06:22:28 +0700

    'ГГ'

--- trash/app/src/main/java/com/example/uiux/Main2Activity.kt
+++ trash/app/src/main/java/com/example/uiux/Main2Activity.kt
@@ -1,23 +1,47 @@
 package com.example.uiux
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.widget.PopupWindow
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import kotlinx.android.synthetic.main.editor_main.*
 
 class Main2Activity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.editor_main)
 
+
+
+
+        btn_rotate.setOnClickListener {
+            val window= PopupWindow(this)
+            val view=layoutInflater.inflate(R.layout.layout_popup,null)
+            window.contentView=view
+            window.showAsDropDown(btn_rotate) }
+
+
+        btn_scaling.setOnClickListener {
+            val window= PopupWindow(this)
+            val view=layoutInflater.inflate(R.layout.scale_popup,null)
+            window.contentView=view
+            window.showAsDropDown(btn_rotate) }
+
+
+
         btn_filters.setOnClickListener {
 
-         val filters=BottomSheetDialog(this)
-        val view=layoutInflater.inflate(R.layout.filters_layout,null)
+            val filters=BottomSheetDialog(this)
+            val view=layoutInflater.inflate(R.layout.filters_layout,null)
             filters.setContentView(view)
             filters.show()
         }
+
+
     }
+
+
 }
--- trash/app/src/main/java/com/example/uiux/MainActivity.kt
+++ trash/app/src/main/java/com/example/uiux/MainActivity.kt
@@ -1,22 +1,40 @@
 package com.example.uiux
-
+import android.Manifest
 import android.app.Activity
+import android.content.ContentValues
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
+import android.database.Cursor
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
 import android.os.Bundle
-import android.widget.PopupMenu
+import android.provider.MediaStore
+import android.util.DisplayMetrics
 import android.widget.PopupWindow
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.editor_main.*
-import java.util.jar.Manifest
+import kotlin.math.roundToInt
+
+
+class roflan(){
+
+    companion object{lateinit var bitmap: Bitmap}
+
+}
 
 class MainActivity : AppCompatActivity() {
-
+    private val PICTURE_RESULT: Int = 1001
+    var imageUri: Uri? = null
+    private val IMAGE_PICK_CODE = 1;
+    private val CAMERA = 2;
+    private val CAMERA_PERMISSION_CODE = 100;
+    private val STORAGE_PERMISSION_CODE = 101;
+    private lateinit var gbmap: Bitmap
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,81 +42,180 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-        main_card.setOnClickListener{
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-                 if (checkPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE) ==
-                        PackageManager.PERMISSION_DENIED){
-                    //permission denied
-                    val permissions = arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE);
-                    //show popup to request runtime permission
-                    requestPermissions(permissions, PERMISSION_CODE);
-                }
-                else{
-                    //permission already granted
-                    pickImageFromGallery();
-                }
-            }
-            else{
-                //system OS is < Marshmallow
-                pickImageFromGallery();
-            }
-            
+        main_card.setOnClickListener {
+            val intent = Intent(this, Main2Activity::class.java)
+            startActivity(intent)
+
+        }
 
 
-                val intent = Intent(this, Main2Activity::class.java)
-                startActivity(intent)
-            }
+        camera_main.setOnClickListener {
 
 
+            checkPermission(Manifest.permission.CAMERA, CAMERA_PERMISSION_CODE)
+            //permission already granted
+            takePhoto(Manifest.permission.CAMERA);
+
+            val intent = Intent(this, Main2Activity::class.java)
+            val bmap: Bitmap = (image_view.getDrawable() as BitmapDrawable).bitmap
+            val aspectRatio: Float = bmap.height.toFloat() / bmap.width
+            val displayMetrics: DisplayMetrics = resources.displayMetrics
+            val mImageWidth = displayMetrics.widthPixels
+            val mImageHeight = (mImageWidth * aspectRatio).roundToInt()
+            val mBitmap = Bitmap.createScaledBitmap(bmap, mImageWidth, mImageHeight, false)
+            roflan.bitmap = mBitmap
+            startActivity(intent)
 
         }
 
 
 
-
-
-    private fun pickImageFromGallery() {
-        //Intent to pick image
-        val intent = Intent(Intent.ACTION_PICK)
-        intent.type = "image/*"
-        startActivityForResult(intent, IMAGE_PICK_CODE)
     }
 
 
-    companion object {
-        //image pick code
-        private val IMAGE_PICK_CODE = 1000;
-        //Permission code
-        private val PERMISSION_CODE = 1001;
+
+    private fun checkPermission(permission: String, requestCode: Int) {
+
+        // Проверка, если разрешение не предоставлено
+        if (ContextCompat.checkSelfPermission(
+                this@MainActivity,
+                permission
+            )
+            == PackageManager.PERMISSION_DENIED
+        ) {
+            ActivityCompat
+                .requestPermissions(
+                    this@MainActivity, arrayOf(permission),
+                    requestCode
+                )
+        } else {
+            Toast
+                .makeText(
+                    this@MainActivity,
+                    "Permission already granted",
+                    Toast.LENGTH_SHORT
+                )
+                .show()
+        }
     }
 
 
-    //handle requested permission result
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        when(requestCode){
-            PERMISSION_CODE -> {
-                if (grantResults.size >0 && grantResults[0] ==
-                    PackageManager.PERMISSION_GRANTED){
-                    //permission from popup granted
-                    pickImageFromGallery()
-                }
-                else{
-                    //permission from popup denied
-                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
-                }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String?>,
+        grantResults: IntArray
+    ) {
+        super
+            .onRequestPermissionsResult(
+                requestCode,
+                permissions,
+                grantResults
+            )
+        if (requestCode == CAMERA_PERMISSION_CODE) {
+
+            // Проверка, предоставил ли пользователь разрешение или нет.
+            if (grantResults.size > 0
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED
+            ) {
+
+                // Отображение тостового сообщения
+                Toast.makeText(
+                    this@MainActivity,
+                    "Camera Permission Granted",
+                    Toast.LENGTH_SHORT
+                )
+                    .show()
+            } else {
+                Toast.makeText(
+                    this@MainActivity,
+                    "Camera Permission Denied",
+                    Toast.LENGTH_SHORT
+                )
+                    .show()
+            }
+        } else if (requestCode == STORAGE_PERMISSION_CODE) {
+            if (grantResults.size > 0
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED
+            ) {
+                Toast.makeText(
+                    this@MainActivity,
+                    "Storage Permission Granted",
+                    Toast.LENGTH_SHORT
+                )
+                    .show()
+            } else {
+                Toast.makeText(
+                    this@MainActivity,
+                    "Storage Permission Denied",
+                    Toast.LENGTH_SHORT
+                )
+                    .show()
             }
         }
     }
 
 
-    //handle result of picked image
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    fun takePhoto(permission: String){
+        var values = ContentValues()
+        values.put(MediaStore.Images.Media.TITLE, "New Picture")
+        values.put(MediaStore.Images.Media.DESCRIPTION, "From your Camera")
+        imageUri = contentResolver.insert(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values
+        )
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
+        startActivityForResult(intent, PICTURE_RESULT)
+    }
+
+
+    fun pickImageFromGallery(permission: String) {
+        if (ContextCompat.checkSelfPermission(
+                this@MainActivity,
+                permission
+            )
+            == PackageManager.PERMISSION_GRANTED
+        ) {
+            val gallaryintent = Intent(Intent.ACTION_PICK)
+            gallaryintent.type = "image/*"
+            startActivityForResult(gallaryintent, IMAGE_PICK_CODE)
+        }
+    }
+
+
+    override fun onActivityResult(
+        requestCode: Int,
+        resultCode: Int,
+        data: Intent?
+    ) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE){
-            image_view.setImageURI(data?.data)
+        when (requestCode) {
+            PICTURE_RESULT -> if (requestCode == PICTURE_RESULT) if (resultCode == Activity.RESULT_OK) {
+                try {
+                    var thumbnail = MediaStore.Images.Media.getBitmap(
+                        contentResolver, imageUri
+                    )
+                    image_view.setImageBitmap(thumbnail)
+                    var imageurl = getRealPathFromURI(imageUri)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
         }
     }
 
+
+    fun getRealPathFromURI(contentUri: Uri?): String? {
+        val proj = arrayOf(MediaStore.Images.Media.DATA)
+        val cursor: Cursor = managedQuery(contentUri, proj, null, null, null)
+        val column_index: Int = cursor
+            .getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
+        cursor.moveToFirst()
+        return cursor.getString(column_index)
     }
+
+
+
+}
+
 
 
